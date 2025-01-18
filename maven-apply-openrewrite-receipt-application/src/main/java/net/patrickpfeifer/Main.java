c40e492ef28dba5c5c92cc9f8233f31742e048f1
Patrick Pfeifer <patrick@patrickpfeifer.net>
2025-01-18 21:02:53 +0100

    initial commit

---
+++ maven-apply-openrewrite-receipt-application/src/main/java/net/patrickpfeifer/Main.java
@@ -0,0 +1,13 @@
+package net.patrickpfeifer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello application!");
+
+        ReceiptMain.main(args);
+
+        System.out.println(new ReceiptMain().hello());;
+    }
+}
---
+++ maven-apply-openrewrite-receipt-receipt/src/main/java/net/patrickpfeifer/ReceiptMain.java
@@ -0,0 +1,7 @@
+package net.patrickpfeifer;
+
+public class ReceiptMain {
+    public static void main(String[] args) {
+        System.out.println("Hello receipt!");
+    }
+}
